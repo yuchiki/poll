ffea289a1d10a2aaf0a91bd3d494eb40b050aa75
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-04-30 22:23:26 +0900

    tutorial3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,22 @@
-from tokenize import String
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404, HttpResponse
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def details(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
