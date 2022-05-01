5762c9b028e4ca2728ce029f8add97a9892eb256
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-05-02 00:19:08 +0900

    unpublished な　questionのdetailを表示しないようにした

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,6 +21,12 @@ class DetailView(generic.DeleteView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that are not published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
