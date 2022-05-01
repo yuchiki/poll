58fe8cae94a3dd7712891c9e6e4096ff8ae99390
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-05-02 00:06:42 +0900

    add test for index

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 
@@ -10,7 +11,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last fice published questions (not including those set to be published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()) .order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DeleteView):
